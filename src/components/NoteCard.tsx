d2c2e3b89d66fc15f64147678641aa39d5f29ea8
Viviane Queiroz <vivianedsqueiroz@gmail.com>
2024-02-12 19:05:48 -0300

    add modal styles

--- src/components/NoteCard.tsx
+++ src/components/NoteCard.tsx
@@ -1,4 +1,6 @@
 import * as Dialog from "@radix-ui/react-dialog";
+import { formatDistanceToNow } from "date-fns";
+import { X } from "lucide-react";
 
 interface NoteCardsProps {
   date: Date;
@@ -14,7 +16,7 @@ function NoteCard({ date, content }: NoteCardsProps) {
         tabIndex={0}
       >
         <h3 className="text-sm font-medium text-slate-300">
-          {date.toISOString()}
+          {formatDistanceToNow(date)}
         </h3>
         <p className="text-sm leading-6 text-slate-400">{content}</p>
 
@@ -24,8 +26,28 @@ function NoteCard({ date, content }: NoteCardsProps) {
 
       <Dialog.Portal>
         <Dialog.Overlay className="inset-0 fixed bg-black/50" />
-        <Dialog.Content className="fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 max-w-[640px] w-full bg-slate-700 outline-none rounded-md flec flex-col">
-          Hii
+        <Dialog.Content className="fixed overflow-hidden left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 max-w-[640px] w-full h-[60vh] bg-slate-700 outline-none rounded-md flex flex-col">
+          <Dialog.Close className="absolute right-0 top-0 bg-slate-800 p-1.5 text-slate-400 hover:text-slate-100">
+            <X className="size-5" />
+          </Dialog.Close>
+
+          <div className="flex flex-1 flex-col gap-3 p-5">
+            <h3 className="text-sm font-medium text-slate-300">
+              {formatDistanceToNow(date)}
+            </h3>
+            <p className="text-sm leading-6 text-slate-400">{content}</p>
+          </div>
+
+          <button
+            type="button"
+            className="w-full bg-slate-800 py-4 text-center text-slate-300 font-medium outline-none group"
+          >
+            Do you want to{" "}
+            <span className="text-red-400 group-hover:underline">
+              delete this note
+            </span>
+            ?
+          </button>
         </Dialog.Content>
       </Dialog.Portal>
     </Dialog.Root>
