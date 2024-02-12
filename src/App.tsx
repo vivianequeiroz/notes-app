297a4068feef3efd7ef9d2bb4af446c16175c051
Viviane Queiroz <vivianedsqueiroz@gmail.com>
2024-02-12 17:43:20 -0300

    add search bar ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div className="mx-auto max-w-6xl my-12 space-y-6">
+      <form>
+        <input
+          className="w-full bg-transparent text-3xl font-semibold tracking-tight placeholder:text-slate-500 outline-none"
+          placeholder="Search for a note..."
+        />
+      </form>
+
+      <div className="h-px bg-slate-700" />
+
+      <div className="grid grid-cols-3 auto-rows-[250px]"></div>
+    </div>
+  );
 }
 
 export default App;
