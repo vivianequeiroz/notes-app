23d945b39319fe7608010fa09ce7b919faf764a2
Viviane Queiroz <vivianedsqueiroz@gmail.com>
2024-02-12 18:32:07 -0300

    add cards components

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,6 @@
+import NoteCard from "./components/NoteCard";
+import NewNoteCard from "./components/NewNoteCard";
+
 function App() {
   return (
     <div className="mx-auto max-w-6xl my-12 space-y-6">
@@ -5,12 +8,19 @@ function App() {
         <input
           className="w-full bg-transparent text-3xl font-semibold tracking-tight placeholder:text-slate-500 outline-none"
           placeholder="Search for a note..."
+          autoFocus
         />
       </form>
 
       <div className="h-px bg-slate-700" />
 
-      <div className="grid grid-cols-3 auto-rows-[250px]"></div>
+      <div className="grid grid-cols-3 gap-6 auto-rows-[250px]">
+        <NewNoteCard />
+
+        <NoteCard />
+        <NoteCard />
+        <NoteCard />
+      </div>
     </div>
   );
 }
---
+++ src/components/NewNoteCard.tsx
@@ -0,0 +1,12 @@
+function NewNoteCard() {
+  return (
+    <div className="rounded-md bg-slate-700 p-5 space-y-3 overflow-hidden">
+      <h3 className="text-sm font-medium text-slate-200">Add note</h3>
+      <p className="text-sm leading-6 text-slate-400">
+        Record a note that will automatically be converted to text.
+      </p>
+    </div>
+  );
+}
+
+export default NewNoteCard;
